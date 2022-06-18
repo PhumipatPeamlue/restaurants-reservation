3c591877b23f3e2dafe10725a143a689cc82dfdf
PhumipatPeamlue <107468576+PhumipatPeamlue@users.noreply.github.com>
2022-06-18 15:22:22 +0700

    create get request by name and create post request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 class Reservation(BaseModel):
     name : str
@@ -10,10 +11,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurants"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
 
 app = FastAPI()
 
@@ -21,7 +22,21 @@ app = FastAPI()
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    query = collection.find({"name": name})
+    res = {}
+    No = 1
+    for i in query:
+        obj = Reservation()
+        obj.name = i["name"]
+        obj.time = i["time"]
+        obj.table_number = i["table_number"]
+        res[No] = jsonable_encoder(obj)
+        No += 1
+    
+    if len(res) == 0:
+        raise HTTPException(404, "Can't find anyone who reserved by this name.")
+    else:
+        return {"result": res}
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
@@ -29,7 +44,12 @@ def get_reservation_by_table(table: int):
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    pass
+    query = collection.find({"table_number": reservation.table_number})
+    for i in query:
+        if i["time"] <= reservation.time < i["time"] + 1:
+            return {"result": "Already reserved."}
+    collection.insert_one(jsonable_encoder(reservation))
+    return {"result": "Reservation complete!"}
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
