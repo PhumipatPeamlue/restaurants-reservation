777c37f342c6fd719d0c1d6379ce8536f4e7353e
PhumipatPeamlue <107468576+PhumipatPeamlue@users.noreply.github.com>
2022-06-18 16:24:59 +0700

    add put request update reservation

--- main.py
+++ main.py
@@ -73,7 +73,16 @@ def reserve(reservation : Reservation):
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
-    pass
+    query = collection.find({"table_number": reservation.table_number})
+    for i in query:
+        if i["time"] <= reservation.time < i["time"] + 1:
+            return {"result": "The new reservation is not allowed."}
+    
+    find_by = {"name": reservation.name, "table_number": reservation.table_number}
+    new_value = {"$set": {"time": reservation.time}}
+    collection.update_one(find_by, new_value)
+    return {"result": "The new reservation is complete!"}
+
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
